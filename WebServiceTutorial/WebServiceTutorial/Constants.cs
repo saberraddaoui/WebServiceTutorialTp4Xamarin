5771eefe3f5554dc2890eb9f56a427d8e0e5248d
saberraddaoui <saberraddaoui2001@gmail.com>
2022-12-03 23:08:57 +0100

    Ajoutez des fichiers projet.

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServiceTutorial
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint =
+       "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey =
+       "5653a80ccb0c6b52daa70954756aa61e";
+    }
+}
